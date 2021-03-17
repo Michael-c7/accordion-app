4ae4c5bcf5e5fad4ee618ee637dd4eba223bb0c7
Michael Carr <mcarr2468@gmail.com>
2021-03-17 14:56:29 -0500

    V1.2

--- app.js
+++ app.js
@@ -1,11 +1,86 @@
-let header = document.querySelector(".faq__header")
-let iconContainer = document.querySelector(".faq__heading__icon-container")
-let text = document.querySelector(".faq__text")
-
-header.addEventListener("click", _ => {
-    text.classList.toggle("faq__text--active");
+let accordionItem = Array.from(document.getElementsByClassName("accordion__faq"));
 
 
-    iconContainer.classList.toggle("faq__text--active");
-    
-})
+function accordion() {
+
+    for(let i = 0; i < accordionItem.length; i++) {
+        let accordionHeader = accordionItem[i].children[0];
+        let accordionText = accordionItem[i].children[1];
+        let firstBarInIconBtn = accordionItem[i].children[0].children[1].children[0];
+
+       accordionHeader.addEventListener("click", _ => {
+           accordionText.classList.toggle("faq__text--active")
+           accordionItem[i].classList.toggle("accordion__faq--active")
+           firstBarInIconBtn.classList.toggle("bar-minus")
+       })
+    }
+}
+
+accordion()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let header = document.querySelector(".faq__header")
+// let iconContainer = document.querySelector(".faq__heading__icon-container")
+// let text = document.querySelector(".faq__text")
+// let accordionFaq = document.querySelector(".accordion__faq")
+// let accordionFaqs = document.querySelector(".accordion__faqs")
+
+// function openAccordion(event) {
+//     let closestHeader = event.target.closest(".faq__header");
+//     let closestText = event.target.closest(".faq__text");
+
+//     if(event.target === closestHeader)  {
+//         closestText.classList.toggle("faq__text--active");
+
+//     }
+
+// }
+
+// accordionFaqs.addEventListener("click", openAccordion)
